87b7e278c609f1f6113d176de2c0542f9b043248
Jose Melero <jmelero08@gmail.com>
2021-10-06 14:52:50 -0600

    added store

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
 import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    this.props.getCurrentUser()
-  }
-  
+  // componentDidMount() {
+  //   this.props.getCurrentUser()
+  // }
+
   render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      "Hello"
+    );
   }
 }
 
